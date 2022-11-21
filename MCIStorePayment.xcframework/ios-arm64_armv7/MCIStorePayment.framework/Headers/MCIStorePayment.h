7a41002a094f110b9c5c2d3505554cbfe8d49d12
Thais Sadami <thaissadami@gmail.com>
2022-11-21 18:40:46 -0300

    [Pod] - Version 1.9.12

---
+++ MCIStorePayment.xcframework/ios-arm64_armv7/MCIStorePayment.framework/Headers/MCIStorePayment.h
@@ -0,0 +1,19 @@
+//
+//  MCIStorePayment.h
+//  MCIStorePayment
+//
+//  Created by Gustavo Cassab on 07/08/19.
+//  Copyright © 2019 Merci. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MCIStorePayment.
+FOUNDATION_EXPORT double MCIStorePaymentVersionNumber;
+
+//! Project version string for MCIStorePayment.
+FOUNDATION_EXPORT const unsigned char MCIStorePaymentVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MCIStorePayment/PublicHeader.h>
+
+
